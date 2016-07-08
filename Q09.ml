16ec93058086bde732af2bdfb49766bbddd983a2
Leonardo Augusto <leonerd12@gmail.com>
2016-07-08 15:09:03 -0400

    [ADJUST] Finalizado Q09.ml
    Signed-off-by: Leonardo Augusto <leonerd12@gmail.com>

--- Q09.ml
+++ Q09.ml
@@ -1,80 +1,93 @@
-fun somar(nil) = 0
-	| somar(x::xs) = x + somar(xs);
-
-fun podeJogar((e1, e2, e3, e4 : (int * int) list), lista_pedras: (int * int) list) = 
-let
-    fun g([]) = []
-	| g(x::xs) = if x = nil then [(6,6)]@g(xs) else hd(x)::g(xs);
-
-	val lista = e1 :: e2 :: e3 :: e4 :: [];
-
-	fun soma(_,nil) = false
-    | soma(Y as (z,zs) : int*int, (x1,x2)::xs)  = 
-         if (z = x1) orelse (zs = x1) then true  
-         	else soma(Y,xs);
-
-	fun comparar((c, d)::xs: (int * int) list) = 
-		if (soma((c, d), g(lista))) then (c, d) :: comparar(xs) 
-		else comparar(xs)
-	| comparar(nil) = nil
-
-in
-	comparar(lista_pedras)
-end;
-
+(* Questão 9 - Daniel Lopes Zordan Filho e  Leonardo Augusto Picanço Barreto *)
+(*Para resolver a questão 9 foi necessario usar a solução quase igual a da 8, mudando apenas algumas funções a mais, que se segue abaixo*)
 fun g([]) = []
-	| g(x::xs) = if x = nil then g(xs) else hd(x)::g(xs);
-
+    | g(x::xs) = 
+          if x = nil then g(xs) 
+                            else hd(x)::g(xs);
+(*função que pega o primeiro parâmetro de cada pedra*)
 fun h(nil) = nil
 |h((x1 : int, x2 : int) :: xs)  = x1::h(xs);
 
+(*função que recebe um estado e uma pedra, mapeia se pode jogar e retorna a pedra no lugar jogado*)
 fun novoEstado((e1, e2, e3, e4: (int * int) list), (a: int, b: int)) = 
 let
 
-	val lista = e1 :: e2 :: e3 :: e4 :: [];
+    val lista = e1 :: e2 :: e3 :: e4 :: [];
+    val l = h(g(lista));
 
-	val l = h(g(lista));
+    (*função que compara se pode jogar uma pedra no ramo, e se puder, retorna o ramo com a pedra invertida como ponta *)
+    fun novaPonta((x1: int, x2: int), x::xs, cont: int) = 
+                                                  if x1 = x then ((x2, x1)::nil, cont) 
+                                                            else if x2 = x then ((x1, x2)::nil, cont) 
+                                                                           else novaPonta((x1, x2), xs, cont+1)
+    | novaPonta(_, nil, cont) = (nil, cont);
 
-	fun novaPonta((x1: int, x2: int), x::xs, cont: int) = 
-		if x1 = x then ((x2, x1)::nil, cont) 
-		else if x2 = x then ((x1, x2)::nil, cont) 
-		else novaPonta((x1, x2), xs, cont+1)
-	| novaPonta(_, nil, cont) = (nil, cont);
+    val teste = novaPonta((a, b), l, 1);
 
-	val teste = novaPonta((a, b), l, 1);
+    val c = #2 teste;
+    val p = #1 teste;
 
-	val c = #2 teste;
-	val p = #1 teste;
+    (*função retorna um novo estado do jogo com a pedra jogada na posição correta*)
+    fun returnNewState(x1, x2, x3, x4: (int * int) list, nil) = (x1, x2, x3, x4)
+    | returnNewState(x1, x2, x3, x4, k) = 
+                                   if(c = 1) then (hd(k)::x1, x2, x3, x4)
+                                             else if(c = 2) then (x1, hd(k)::x2, x3, x4)
+                                                            else if(c = 3) then (x1, x2, hd(k)::x3, x4)
+                                                                           else (x1, x2, x3, hd(k)::x4);
 
-	fun returnNewState(x1, x2, x3, x4: (int * int) list, nil) = (x1, x2, x3, x4)
-	| returnNewState(x1, x2, x3, x4, k) = 
-		if(c = 1) then (hd(k)::x1, x2, x3, x4)
-		else if(c = 2) then (x1, hd(k)::x2, x3, x4)
-		else if(c = 3) then (x1, x2, hd(k)::x3, x4)
-		else (x1, x2, x3, hd(k)::x4);
-
-	val r = returnNewState(e1, e2, e3, e4, p)
-	val la = #1 r :: #2 r :: #3 r :: #4 r :: nil
+    val r = returnNewState(e1, e2, e3, e4, p)
+    val la = #1 r :: #2 r :: #3 r :: #4 r :: nil
 in
-	la
+    la
 end;
 
+(*função que soma as pontas dos ramos, se for carroça, soma os dois naipes*)
+fun pegaSoma(nil) = 0  
+       |pegaSoma((x1 : int, x2 : int) :: xs)  = 
+            if(x1 = x2) then x1 + x2 + pegaSoma(xs) 
+                        else x1 + pegaSoma(xs);
+(*função que, usa as outras funções e detecta a pedra que marca mais pontos, e retorna essa*)
 fun pedrasPont(e1, e2, e3, e4 : (int * int) list, listaPedras: (int * int) list) =
 let
-	val possiveis = podeJogar((e1, e2, e3, e4), listaPedras);
 
-	fun pegarPontas(nil) = nil
-	| pegarPontas(lista) = somar(h(g(lista)));
+    (*função que pegaas pontas do resultado da função "g" de lista*)
+    fun pegarPontas(nil) = nil
+    | pegarPontas(lista) = g(lista);
 
-	fun listar(nil) = nil
-	| listar(cab::cau) = 
-		if(pegarPontas(novoEstado((e1, e2, e3, e4), cab)) < pegarPontas(novoEstado((e1, e2, e3, e4), hd(cau)))) mod 5 = 0) 
-		then cab :: listar(cau) 
-		else listar(cau)
+    (*função que anota os pontos de cada pedra*)
+    fun pontosMarcados(x1, x2, x3, x4, c) = pegaSoma(pegarPontas(novoEstado((x1, x2, x3, x4), c)))
+    (*função que lista quais pedras pontuam ao serem jogadas*)
+    fun listar(nil)    = nil
+    | listar(cab::cau) = 
+    if(pontosMarcados(e1, e2, e3, e4, cab) mod 5 = 0) then (pontosMarcados(e1, e2, e3, e4, cab), cab):: listar(cau) 
+                                                                           else listar(cau)
 
 in
-	listar(possiveis)
+    listar(listaPedras) 
 end; 
 
-(* Caso de teste *)
-pedrasPont([(6,6)],[(2,1)], [(4,5),(5,6)],[(3,6)], [(6,1), (2,2)]); 
+(*Aqui é onde a solução da questão  9 começa, de fato.
+Esta função receberá os pontos*)
+fun nine((listao : (int * (int*int)) list)) = 
+let
+    (*função que forma uma lista com os valores das somas de cada pedra*)
+    fun h(nil) = nil
+    |h((x1 : int, (x2,x3) : (int*int)) :: xs)  = [x1]@(h(xs));
+
+    (*função que mapeia uma lista de inteiros e retorna o maior valor*)
+    fun maior(x, nil) = x
+    | maior(x: int, a::b) = if (x > a) then maior(x, b) 
+                          else maior(a, b);
+
+    val r = h(listao);
+
+    val tt = maior(hd(r), tl(r));
+
+    (*função que compara o primeiro elemento da cabeça de  uma lista (int * (int*int)) com o primeiro da cauda, recurssivamente  *)
+    fun chora(_,nil) = nil
+    |chora(D,lis::liss : (int * (int*int)) list) =
+        if (D = #1(lis)) then ([lis])
+                             else chora(D,liss)
+in
+    chora(tt,listao)
+end;
